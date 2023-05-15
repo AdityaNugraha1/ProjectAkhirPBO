7102f75389d9041e18d7ff5de22f8842986f45da
AdityaNugraha1 <adit.toyan27@gmail.com>
2023-05-15 19:13:23 +0700

    1

---
+++ src/koneksi/KoneksiDatabase.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+
+/**
+ *
+ * @author LEGION
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class KoneksiDatabase {
+   Connection cn;
+   public static Connection BukaKoneksi(){
+       try {
+           Class.forName("com.mysql.jdbc.Driver");
+           Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/ ", "root", "");
+           return cn;
+       } catch (ClassNotFoundException | SQLException e) {
+           JOptionPane.showMessageDialog(null, e);
+           return null;
+       }
+   }
+}
